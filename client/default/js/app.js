8aeaf7f3db66de1f609f12aa50b99fc88252da7b
john.frizelle@feedhenry.com <john.frizelle@feedhenry.com>
2012-08-24 09:46:16 +0000

    update file /client/default/js/app.js

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$fh.ready(function() {
   $fh.init({}, function() {
     var url = ($fh.app_props.mode === "dev" ? $fh.cloud_props.hosts.debugCloudUrl : $fh.cloud_props.hosts.releaseCloudUrl);
     document.getElementById('serverurl').innerHTML = 'Server URL: ' + url;
